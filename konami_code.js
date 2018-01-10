9001ae968b9534a99814f44d457f2ed4f00d20cd
Randall Harris Haynes <harrishaynes@gmail.com>
2018-01-10 18:31:09 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,8 +1,8 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 function init() {
 
-  const alphabet = [65 66 67];
+  const alphabet = code;
 
   let index = 0;
 
