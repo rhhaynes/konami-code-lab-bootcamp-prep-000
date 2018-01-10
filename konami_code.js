50129c2a422009abecb527e4bde4ccc14df868e9
Randall Harris Haynes <harrishaynes@gmail.com>
2018-01-10 18:47:24 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,9 +1,7 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+const alphabet = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+let index = 0;
 
 function init() {
-
-  const alphabet = code;
-  let index = 0;
 
   function onKeyDownHandler(e) {
     const key = parseInt(e.detail || e.which);
